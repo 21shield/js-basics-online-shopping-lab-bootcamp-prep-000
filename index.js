dfcb849b247d120baa9171c791d877bbf1bc29f0
Netaly Ramirez <21netaly@gmail.com>
2020-03-19 22:45:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,10 +51,8 @@ function removeFromCart(item) {
   for (let i = 0; i <cart.length; i ++){
     if (cart[i].itemName === item){
       cart.splice(i,1)
-    }else{
-      return 'That item is not in your cart.'
     }
-  }
+  }  return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
