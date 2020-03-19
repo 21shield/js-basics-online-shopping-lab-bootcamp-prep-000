5ddf5980d23ea11b2c53fda7c1425b04640b04f3
Netaly Ramirez <21netaly@gmail.com>
2020-03-19 22:31:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+   return cart;
 }
 
 function setCart(c) {
@@ -11,14 +11,39 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ let obj = {}
+ obj.itemName = item
+ obj.itemPrice = Math.floor(Math.random()* 100)
+ cart.unshift(obj)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  let shopee = ['In your cart, you have']
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }else if (cart.length === 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }else{
+    for (let i = 0; i < cart.length; i ++){
+      if (i === cart.length-1){
+        shopee.push(`and ${cart[i].itemName} at $${cart[i].itemPrice}.`)
+      }else{
+        shopee.push(`${cart[i].itemName} at $${cart[i].itemPrice},`)
+      }
+    }return shopee.join(' ')
+  }
 }
 
 function total() {
-  // write your code here
+  //write your code here
+  let totalCost = 0
+  for(let i = 0; i < cart.length; i++){
+    let currentObj = cart[i];
+    totalCost += currentObj.itemPrice;
+  }
+  return totalCost
 }
 
 function removeFromCart(item) {
